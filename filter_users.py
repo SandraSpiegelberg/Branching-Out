a0174de8b8a9106cd40ba09e463fe11f601587c4
Sandras Mac <sandra@air-von-sandras.speedport.ip>
2025-06-27 02:34:47 +0200

    formatting the code and add some comments

--- filter_users.py
+++ filter_users.py
@@ -1,6 +1,11 @@
 import json
 
 def filter_users_by_name(name):
+    """
+    Filtering users by their name from a JSON file.
+    :params: name (str): The name to filter users 
+    :returns: None
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
     
@@ -10,6 +15,11 @@ def filter_users_by_name(name):
         print(user)
 
 def filter_by_age(age):
+    """
+    Filtering users by their age from a JSON file.
+    :params: age (int): The age to filter users 
+    :returns: None
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
     
@@ -19,6 +29,11 @@ def filter_by_age(age):
         print(user)
 
 def filter_by_email(email):
+    """
+    Filtering users by their email from a JSON file.
+    :params: email (str): The email to filter users 
+    :returns: None
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
     
@@ -27,7 +42,12 @@ def filter_by_email(email):
     for user in filtered_users:
         print(user)
 
-if __name__ == "__main__":
+def filter_options():
+    """ 
+    Prompting user for filter options and calling the appropriate function to filtering the entered value of the user.
+    :params: None
+    :returns: None
+    """
     filter_option = input("What would you like to filter by? (Currently, only 'name', 'age' or 'email' is supported): ").strip().lower()
     
     if filter_option == "name":
@@ -41,3 +61,6 @@ if __name__ == "__main__":
         filter_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
+
+if __name__ == "__main__":
+    filter_options()
